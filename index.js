59c3cc3f254575687f1c3185f5b49e70a1a09a97
Jean Viegas <jeanmelg@gmail.com>
2022-04-25 16:02:37 -0300

    fix lint

--- index.js
+++ index.js
@@ -14,20 +14,18 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf8'))
+const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf8'));
 
 // 1 - Crie o endpoint GET /talker
-app.get('/talker', (req, res) => {
-  return res.status(200).json(talkers);
-})
+app.get('/talker', (req, res) => res.status(200).json(talkers));
 
 // Crie o endpoint GET /talker/:id
 app.get('/talker/:id', (req, res) => {
-  const { id } = req.params
-  const talker = talkers.find(talker => talker.id === Number(id))
-  if (talker) return res.status(200).json(talker)
-  return res.status(404).json({message: 'Pessoa palestrante não encontrada'})
-})
+  const { id } = req.params;
+  const talker = talkers.find((elem) => elem.id === Number(id));
+  if (talker) return res.status(200).json(talker);
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
 
 app.listen(PORT, () => {
   console.log('Online');
