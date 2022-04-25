b6015d4417efc8fbff358eed58389c46c72a8148
Jean Viegas <jeanmelg@gmail.com>
2022-04-25 15:57:30 -0300

    req 02

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const fs = require('fs');
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -11,6 +13,21 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf8'))
+
+// 1 - Crie o endpoint GET /talker
+app.get('/talker', (req, res) => {
+  return res.status(200).json(talkers);
+})
+
+// Crie o endpoint GET /talker/:id
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params
+  const talker = talkers.find(talker => talker.id === Number(id))
+  if (talker) return res.status(200).json(talker)
+  return res.status(404).json({message: 'Pessoa palestrante não encontrada'})
+})
 
 app.listen(PORT, () => {
   console.log('Online');
